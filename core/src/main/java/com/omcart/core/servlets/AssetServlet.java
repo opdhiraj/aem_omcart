9695b3315e457ead14ae71e485f86effb7e10ce5
Om Prakash Dhiraj <om.dhiraj@ad.infosys.com>
2023-05-25 15:02:07 +0530

    assetservlet changes

--- core/src/main/java/com/omcart/core/servlets/AssetServlet.java
+++ core/src/main/java/com/omcart/core/servlets/AssetServlet.java
@@ -1,19 +1,26 @@
 package com.omcart.core.servlets;
 
 
-import com.day.crx.JcrConstants;
+import com.day.cq.search.PredicateGroup;
+import com.day.cq.search.Query;
+import com.day.cq.search.QueryBuilder;
+import com.day.cq.search.result.Hit;
+import com.day.cq.search.result.SearchResult;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jcr.Session;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component(service = Servlet.class,
         property = {
@@ -32,17 +39,51 @@ import java.io.IOException;
 //      //  resourceTypes = "omcart/components/structure/page"
 //
 //)
+
+
+
 public class AssetServlet extends SlingSafeMethodsServlet {
+
+
+    private  static final String QUERY_PATH="/content/dam/omcart";
 
     private static final Logger LOGGER= LoggerFactory.getLogger(AssetServlet.class);
 
     @Override
     protected void doGet(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws ServletException, IOException {
-        Resource resource=request.getResource();
-        LOGGER.debug("AssetServlet test=test=tttttt");
+        //        Resource resource=request.getResource();
+        ResourceResolver resourceResolver=request.getResourceResolver();
+        Session session=resourceResolver.adaptTo(Session.class);
+        QueryBuilder qb=resourceResolver.adaptTo(QueryBuilder.class);
 
-        response.setContentType("text/plain");
-        response.getWriter().write("page title "+ resource.getValueMap().get(JcrConstants.JCR_PRIMARYTYPE));
+        Map<String, String>predicate=new HashMap<>();
+        predicate.put("path",QUERY_PATH);
+        predicate.put("type","dam:Asset");
+        Query query=null;
+
+        try {
+
+            query=qb.createQuery(PredicateGroup.create(predicate),session);
+//            LOGGER.debug("query {}",query.getResult());
+
+        }catch (Exception e){
+            LOGGER.error("err",e);
+        }
+        SearchResult searchResult=query.getResult();
+
+        for (Hit hit :searchResult.getHits()){
+            try {
+
+              LOGGER.debug("single hit--> {} ,</br>res::::: {} ",hit.getPath(),hit.getResource().getValueMap().keySet());
+
+            }catch (Exception e){
+                LOGGER.error("err {} ",e);
+            }
+        }
+//        LOGGER.debug("AssetServlet {}", resource.getName());
+
+        response.setContentType("text/html");
+//        response.getWriter().write("page title "+" <h1>"+resource.getName()+"</h1>" );
     }
 
 
